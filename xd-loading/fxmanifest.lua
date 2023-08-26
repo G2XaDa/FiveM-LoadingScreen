965aea1c1e2621028ebb7380250bd902e472d49c
XaDa- <77050474+G2XaDa@users.noreply.github.com>
2023-08-26 23:02:00 +0300

    Add files via upload

---
+++ xd-loading/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'XaDa-'
+description 'XaDa- loading screen'
+version '0.1'
+
+files {
+    'index.html',
+    'style.css',
+    'main.js',
+    'images/faker.png',
+    'images/song.mp3',
+    'images/storm.png',
+    'images/wallpaper.jpg',
+    'images/xada.jpg',
+    'images/xada-logo.png'
+}
+
+loadscreen_cursor 'yes'
+
+
+loadscreen 'index.html'
---
+++ xd-loading/main.js
@@ -0,0 +1,27 @@
+
+//Icon change//
+
+let changeIcon = function(icon) {
+    icon.classList.toggle('fa-volume-off')
+}
+
+//Icon change end//
+
+//Opends Discord link
+
+   function discord(){
+     window.open("https://dsc.gg/allianceofevil/");
+   }
+
+//Opends Web link
+
+function web(){
+  window.open("https://dsc.gg/allianceofevil/");
+}
+
+//Opends Youtube link
+
+function youtube(){
+  window.open("https://www.youtube.com/channel/UC-cgg12QO4fA1s9Z2kUW3ew");
+}
+
